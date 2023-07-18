56e565199bdda722312d21382696a33982cdb1a2
Sarah French <sarah.french@hashicorp.com>
2023-07-18 23:12:53 +0100

    Try different way to defined returned type

--- .teamcity/components/generated/vcs_root.kt
+++ .teamcity/components/generated/vcs_root.kt
@@ -3,9 +3,8 @@
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 
-fun getProviderRepository(branch: String) : Class<out GitVcsRoot>{
-    val branchRef = branch
-    return object providerRepository : GitVcsRoot({
+fun getProviderRepository(branchRef: String) : GitVcsRoot{
+    return GitVcsRoot {
         name = "terraform-provider-google"
         url = "https://github.com/hashicorp/terraform-provider-google.git"
         agentCleanPolicy = AgentCleanPolicy.ON_BRANCH_CHANGE
@@ -13,5 +12,5 @@ fun getProviderRepository(branch: String) : Class<out GitVcsRoot>{
         branchSpec = "+:*"
         branch = branchRef
         authMethod = anonymous()
-    })
+    }
 }
