b9b43b803a6ae91498a5aa53133423f83c47d0bb
Sarah French <sarah.french@hashicorp.com>
2023-07-18 23:23:27 +0100

    Address error `mandatory 'id' property is not specified`

--- .teamcity/components/generated/vcs_root.kt
+++ .teamcity/components/generated/vcs_root.kt
@@ -6,8 +6,11 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot.AgentCleanFilesPolicy
 
 
 fun getProviderRepository(branchRef: String) : GitVcsRoot{
+    val repoName = "terraform-provider-google"
+    val vcsId = repoName + branchRef
     return GitVcsRoot {
-        name = "terraform-provider-google"
+        id(vcsId)
+        name = repoName
         url = "https://github.com/hashicorp/terraform-provider-google.git"
         agentCleanPolicy = ON_BRANCH_CHANGE
         agentCleanFilesPolicy = ALL_UNTRACKED
