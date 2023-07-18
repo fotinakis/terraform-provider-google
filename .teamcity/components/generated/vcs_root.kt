414dc4a237c55ff0bfe9d2d9ac257093853ce458
Sarah French <sarah.french@hashicorp.com>
2023-07-18 23:14:21 +0100

    Try address errors like `Unresolved reference:AgentCleanPolicy`

--- .teamcity/components/generated/vcs_root.kt
+++ .teamcity/components/generated/vcs_root.kt
@@ -1,14 +1,16 @@
 // this file is auto-generated with mmv1, any changes made here will be overwritten
 
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot.AgentCleanPolicy.ON_BRANCH_CHANGE
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot.AgentCleanFilesPolicy.ALL_UNTRACKED
 
 
 fun getProviderRepository(branchRef: String) : GitVcsRoot{
     return GitVcsRoot {
         name = "terraform-provider-google"
         url = "https://github.com/hashicorp/terraform-provider-google.git"
-        agentCleanPolicy = AgentCleanPolicy.ON_BRANCH_CHANGE
-        agentCleanFilesPolicy = AgentCleanFilesPolicy.ALL_UNTRACKED
+        agentCleanPolicy = ON_BRANCH_CHANGE
+        agentCleanFilesPolicy = ALL_UNTRACKED
         branchSpec = "+:*"
         branch = branchRef
         authMethod = anonymous()
