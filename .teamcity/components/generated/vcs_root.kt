6ea8c3cf8dce32dc63210d61f71f0cfefce8b051
Sarah French <sarah.french@hashicorp.com>
2023-07-19 10:09:32 +0100

    urgh

--- .teamcity/components/generated/vcs_root.kt
+++ .teamcity/components/generated/vcs_root.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 fun getProviderRepository(branchRef: String) : GitVcsRoot {
 
-    return object providerRepository : GitVcsRoot({
+    return GitVcsRoot({
         id = "terraform-provider-google"
         name = "terraform-provider-google"
         url = "https://github.com/hashicorp/terraform-provider-google.git"
