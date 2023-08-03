34f3521dd84859b6908ee7f2c3ec996ad7010ebf
Sarah French <sarah.french@hashicorp.com>
2023-08-03 12:13:34 +0100

    Swap to more maintainable workaround

--- .teamcity/components/generated/build_components.kt
+++ .teamcity/components/generated/build_components.kt
@@ -47,12 +47,12 @@ fun BuildSteps.SetGitCommitBuildId() {
     step(ScriptBuildStep {
         name = "Set build id as shortened git commit hash"
         scriptContent = """
-        GIT_HASH=%system.build.vcs.number%
-        GIT_HASH_SHORT=£{GIT_HASH:0:7}
-        echo \"##teamcity[buildNumber '£{GIT_HASH_SHORT}']\"
-        """.replace('£', '$').trimIndent()
-        // Replace approach above is a way to navigate Kotlin string issue,
-        // whose mitigations cause problems in TeamCity, see:
+            GIT_HASH=%system.build.vcs.number%
+            GIT_HASH_SHORT=${'$'}{GIT_HASH:0:7}
+            echo \"##teamcity[buildNumber '${'$'}{GIT_HASH_SHORT}']\"
+        """.trimIndent()
+        // ${'$'} is required to allow creating a script in TeamCity that contains
+        // parts like ${GIT_HASH_SHORT} without having Kotlin syntax issues. For more info see:
         // https://youtrack.jetbrains.com/issue/KT-2425/Provide-a-way-for-escaping-the-dollar-sign-symbol-in-multiline-strings-and-string-templates
     })
 }
