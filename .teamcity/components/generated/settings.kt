ed1a8710557df95c4842cfb4aafe4a730ddfe270
Sarah French <sarah.french@hashicorp.com>
2023-08-03 12:54:30 +0100

    edit CRON for testing

--- .teamcity/components/generated/settings.kt
+++ .teamcity/components/generated/settings.kt
@@ -6,7 +6,7 @@
 // this file is auto-generated with mmv1, any changes made here will be overwritten
 
 // specifies the default hour (UTC) at which tests should be triggered, if enabled
-var defaultStartHour = 4
+var defaultStartHour = 12 // edited
 
 // specifies the default level of parallelism per-service-package
 var defaultParallelism = 12
@@ -15,7 +15,7 @@ var defaultParallelism = 12
 var defaultTerraformCoreVersion = "1.2.5"
 
 // This represents a cron view of days of the week
-const val defaultDaysOfWeek = "1-3,5-7" // All nights except Thursday for GA; feature branch testing happens on Thursdays
+const val defaultDaysOfWeek = "*" // edited
 
 // Cron value for any day of month
 const val defaultDaysOfMonth = "*"
